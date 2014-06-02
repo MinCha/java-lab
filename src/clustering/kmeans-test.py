37241dec410c8a5941e73b60c3daa5807676a2dd
Vayne <Vayne@Vayneui-MacBook-Pro.local>
2014-06-02 17:13:26 +0900

    Refactor with studying Python

---
+++ src/clustering/kmeans-test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import kmeans
+import unittest
+
+class TestKMeans(unittest.TestCase):
+	def test_point_can_compute_eucliean_distance(self):
+		result = kmeans.Point(0,0).edistance(kmeans.Point(2,2))
+		self.assertEqual(2.83,round(result,2))
+
+	def test_point_can_compare(self):
+		self.assertTrue(kmeans.Point(1,1) == kmeans.Point(1,1))
+		self.assertFalse(kmeans.Point(2,1) == kmeans.Point(1,1))
+
+	def test_point_should_return_correct_repr(self):
+		p = kmeans.Point(1,1)
+		other_p = eval('kmeans.' + repr(p))
+		self.assertTrue(p == other_p)		
+
+	def test_point_can_be_added_to_cluster(self):
+		c = kmeans.Cluster(kmeans.Point(1,1))
+		c.add(kmeans.Point(2,2))
+		self.assertEquals(kmeans.Point(2,2), c.child[0])
+
+	def test_cluster_can_compare_with_root(self):
+		c = kmeans.Cluster(kmeans.Point(1,1))
+		self.assertTrue(c.same_root(kmeans.Point(1,1)))
+		self.assertFalse(c.same_root(kmeans.Point(1,2)))
+
+	def test_cluster_can_compute_mean_vector(self):
+		c = kmeans.Cluster(kmeans.Point(1,1))
+		c.add(kmeans.Point(1.5,2))
+		c.add(kmeans.Point(3,4))
+		self.assertEquals(kmeans.Point(1.8,2.3), c.mean_vector())
+
+if __name__ == '__main__':
+	unittest.main()
--- src/clustering/kmeans.py
+++ src/clustering/kmeans.py
@@ -3,19 +3,53 @@ import math
 
 class Point:
 	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+		self.x = float(x)
+		self.y = float(y)
+
 	def __eq__(self, other):
         	if isinstance(other, self.__class__):
 			return self.__dict__ == other.__dict__
+
 	def __ne__(self, other):
         	return not self.__eq__(other)
+
 	def __str__(self):
 		return repr(self);
+
 	def __repr__(self):
 		return "Point(%s,%s)" % (self.x,self.y)
+
 	def edistance(self, point):
 		return math.sqrt(pow(self.x - point.x, 2) + pow(self.y - point.y, 2));
+
+
+class Cluster:
+	def __init__(self, root, child=None):
+		self.root = root
+		self.child = child
+		if (self.child == None): 
+			self.child = []
+
+	def __str__(self):
+		return repr(self);
+
+	def __repr__(self):
+		return "Cluster(%s,%s)" % (self.root,self.child)
+
+	def add(self, child):
+		self.child.append(child)
+
+	def same_root(self, other):
+		return self.root == other
+
+	def all(self): 
+		return [self.root] + self.child
+	
+	def mean_vector(self):
+		x = reduce(lambda i, x: i+x, [p.x for p in self.all()])
+		y = reduce(lambda i, y: i+y, [p.y for p in self.all()])
+		size = len(self.child) + 1
+		return Point(round(x / size,1), round(y / size,1))
 
 def furthest(p):
 	combi = [[x,y] for i,x in enumerate(p) for y in p[i:len(p)] if x != y]
@@ -24,11 +58,11 @@ def furthest(p):
 	return score[len(score)-1].values()[0]
 
 def clustering(c, p):
-	result = {repr(eachc):[] for eachc in c}
+	result = {repr(eachc): Cluster(eachc) for eachc in c}
 	p = [x for x in p if x not in c]
 	for eachp in p:
-		result[repr(closestcluster(eachp,c))].append(eachp)		
-	print result
+		result[repr(closestcluster(eachp,c))].add(eachp)		
+	return result
 
 def closestcluster(p, c):
 	result = None
@@ -41,4 +75,4 @@ def closestcluster(p, c):
 p = ([Point(1,1), Point(1.5,2), Point(3,4), Point(5,7), Point(3.5,5.0),\
 	Point(4.5,5.0), Point(3.5,4.5)])
 initial = furthest(p)
-clustering(initial, p)
+print clustering(initial, p)
