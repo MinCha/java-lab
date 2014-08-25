2cbfeaf6943495a490f16e5b5ac736ddcc93f42c
Vayne <minslovey@gmail.com>
2014-08-25 16:10:04 +0900

    설명할 만한 부분 메모 추가

--- nextnote/src/main/java/nextnote/NextNote.java
+++ nextnote/src/main/java/nextnote/NextNote.java
@@ -4,8 +4,9 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+//TODO [차민창] 학습방법과 추상화에 대한 얘기(닮은 얘기)
+//TODO [차민창] 접근제어자의 의미  
 //TODO 노트 추가 시 폰트 사이즈 어떻게 할꺼야?
-//TODO 차민창 접근제어자의 의미  
 public class NextNote {	
 	public List notes = new LinkedList();
 
@@ -15,7 +16,7 @@ public class NextNote {
 	}
 
 	public Note findNote(int id) {
-		//TODO 노트를 넣었는데 왜 다시 캐스팅을 해야 하나? -> 제네릭으로 이어가면 좋을 듯
+		//TODO [차민창] 노트를 넣었는데 왜 다시 캐스팅을 해야 하나? -> 제네릭으로 이어가면 좋을 듯
 		return (Note) notes.get(id);
 	}
 
